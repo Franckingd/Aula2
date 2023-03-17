ce3d88fcef9c3b10beea388cb9ee446543d4307e
Franckingd <Ibellyfranca150@gmail.com>
2023-03-17 09:27:05 +0900

    Modificação \(erro ELSE IF\)

--- www/components/motor.js
+++ www/components/motor.js
@@ -7,18 +7,20 @@ function calcIMC() {
 
     imc = (massa / (altura * altura)).toFixed(2)
 
-    document.getElementById('resultado').textContent = nome+(", seu IMC é: ")+imc
 
-    if(imc < 18,5){
-        document.getElementById('tabela').textContent = ("Você está abaixo do peso")
+    if(imc <= 18,5){
+        document.getElementById('tabela').textContent = nome+(", seu IMC é: ")+imc+(". Você está abaixo do peso")
     }
-    else if(imc > 18,5 && imc < 24,9){
-        document.getElementById('tabela').textContent = ("Você está no peso ideal")
+    else if(imc > 18,5 && imc <= 24,9){
+        document.getElementById('tabela').textContent = nome+(", seu IMC é: ")+imc+(". Você está no peso ideal")
     }
-    else if(imc > 24,9 && imc < 29,9){
-        document.getElementById('tabela').textContent = ("Você está com sobrepeso")
+    else if(imc > 24,9 && imc <= 29,9){
+        document.getElementById('tabela').textContent = nome+(", seu IMC é: ")+imc+(". Você está com sobrepeso")
     }
-    else if(imc > 29,9){
-        document.getElementById('tabela').textContent = ("Você está com obesidade")
+    else if(imc > 29,9 && imc <= 40){
+        document.getElementById('tabela').textContent = nome+(", seu IMC é: ")+imc+(". Você está com obesidade")
+    }
+    else{
+        document.getElementById('tabela'),textContent = nome+(", seu IMC é: ")+imc+(". Gordo")
     }
 }
