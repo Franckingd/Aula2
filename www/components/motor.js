ebba996af69bbfb4f80fa450c14e866a660a8b28
Franckingd <Ibellyfranca150@gmail.com>
2023-04-03 23:08:27 +0900

    correto

--- www/components/motor.js
+++ www/components/motor.js
@@ -8,16 +8,16 @@ function calcIMC() {
     imc = (massa / (altura * altura)).toFixed(2)
 
 
-    if(imc <= 18,5){
+    if(imc <= 18.5){
         document.getElementById('tabela').textContent = nome+(", seu IMC é: ")+imc+(". Você está abaixo do peso")
     }
-    else if(imc > 18,5 && imc <= 24,9){
+    else if(imc > 18.5 && imc <= 24.9){
         document.getElementById('tabela').textContent = nome+(", seu IMC é: ")+imc+(". Você está no peso ideal")
     }
-    else if(imc > 24,9 && imc <= 29,9){
+    else if(imc > 24.9 && imc <= 29.9){
         document.getElementById('tabela').textContent = nome+(", seu IMC é: ")+imc+(". Você está com sobrepeso")
     }
-    else if(imc > 29,9 && imc <= 40){
+    else if(imc > 29.9 && imc <= 40){
         document.getElementById('tabela').textContent = nome+(", seu IMC é: ")+imc+(". Você está com obesidade")
     }
     else{
